e20efc6c8f67018540256ac27f5c756c6c64fc81
cahyacoding <cahya.bgg@gmail.com>
2025-05-31 09:23:20 +0700

    chore: moved faskes and departement models
    mindahin aja, ga ngapa2in paling ganti import

--- management/models.py
+++ management/models.py
@@ -2,6 +2,9 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
+from authentication.models import Departemen,Faskes
+
 from .choices import * 
 
 # Create your models here.
@@ -168,34 +171,6 @@ class NakesSkill(models.Model):
             return 'profesi_umum'
 
 
-class Faskes(models.Model):
-    faskes_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nama_faskes = models.CharField(max_length=255)
-    alamat = models.TextField(blank=True, null=True)
-    tipe_faskes = models.CharField(max_length=100, blank=True, null=True,
-                                   help_text="Contoh: Rumah Sakit, Puskesmas, Klinik")
-    nomor_telepon = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    
-    # Mungkin ada user yang mengelola Faskes (misal: admin Faskes)
-    # admin_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='managed_faskes')
-
-    def __str__(self):
-        return self.nama_faskes
-    
-
-class Departemen(models.Model):
-    departemen_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    faskes = models.ForeignKey(Faskes, on_delete=models.CASCADE, related_name='departemen')
-    nama_departemen = models.CharField(max_length=255)
-    jam_buka = models.TimeField(blank=True, null=True)
-    jam_tutup = models.TimeField(blank=True, null=True)
-    
-    
-
-    def __str__(self):
-        return f"{self.nama_departemen} ({self.faskes.nama_faskes})"
-  
     
 class Shift(models.Model):
     shift_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
