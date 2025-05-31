b80d5999784946b81678a276c09ef099ff1940bc
albyarto <albyarto.07@gmail.com>
2025-05-31 18:12:07 +0700

    feat: refactor faskes models

--- authentication/models.py
+++ authentication/models.py
@@ -1,19 +1,50 @@
 import uuid
 from django.db import models
 
-# Create your models here.
 class Faskes(models.Model):
-    faskes_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    faskes_id_internal = models.CharField(max_length=50, unique=True, primary_key=True, help_text="ID unik internal Faskes Partner")
     nama_faskes = models.CharField(max_length=255)
-    alamat = models.TextField(blank=True, null=True)
-    tipe_faskes = models.CharField(max_length=100, blank=True, null=True,
-                                   help_text="Contoh: Rumah Sakit, Puskesmas, Klinik")
-    nomor_telepon = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    
-    # Mungkin ada user yang mengelola Faskes (misal: admin Faskes)
-    # admin_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='managed_faskes')
+    jenis_faskes = models.CharField(max_length=100) # Bisa pakai choices
 
+    # Alamat
+    alamat_jalan = models.CharField(max_length=255)
+    alamat_kelurahan_desa = models.CharField(max_length=100)
+    alamat_kecamatan = models.CharField(max_length=100)
+    alamat_kota_kabupaten = models.CharField(max_length=100)
+    alamat_provinsi = models.CharField(max_length=100)
+    alamat_kode_pos = models.CharField(max_length=10, blank=True)
+    
+    koordinat_latitude = models.FloatField(null=True, blank=True)
+    koordinat_longitude = models.FloatField(null=True, blank=True)
+    
+    nomor_izin_operasional = models.CharField(max_length=100, blank=True)
+    
+    # Kontak PIC
+    pic_nama = models.CharField(max_length=255, blank=True)
+    pic_telepon = models.CharField(max_length=20, blank=True)
+    pic_email = models.EmailField(blank=True)
+    
+    # Jam Operasional
+    ops_hari = models.CharField(max_length=100, blank=True) # "Senin - Jumat", "Setiap Hari"
+    ops_jam_buka = models.TimeField(null=True, blank=True)
+    ops_jam_tutup = models.TimeField(null=True, blank=True)
+    ops_catatan = models.TextField(blank=True)
+    
+    # Kapasitas (bisa JSONField atau field terpisah)
+    kapasitas_info_json = models.JSONField(null=True, blank=True, help_text='{"tempat_tidur": 100, "poli_aktif": 5}')
+    
+    layanan_unggulan_json = models.JSONField(null=True, blank=True, help_text='["UGD", "Poli Anak"]') # List of strings
+    
+    # Data Historis
+    historis_kunjungan_bulanan_json = models.JSONField(null=True, blank=True)
+    rata_kunjungan_harian_seminggu_json = models.JSONField(null=True, blank=True)
+    pola_kunjungan_layanan_json = models.JSONField(null=True, blank=True)
+    periode_puncak_diketahui_json = models.JSONField(null=True, blank=True) # List of strings
+    historis_klb_lokal_json = models.JSONField(null=True, blank=True) # List of dicts
+    baseline_staffing_json = models.JSONField(null=True, blank=True)
+    
+    is_active_partner = models.BooleanField(default=True)
+    joined_date = models.DateField(auto_now_add=True)
     def __str__(self):
         return self.nama_faskes
     
@@ -26,7 +57,6 @@ class Departemen(models.Model):
     jam_tutup = models.TimeField(blank=True, null=True)
     
     
-
     def __str__(self):
         return f"{self.nama_departemen} ({self.faskes.nama_faskes})"
   
